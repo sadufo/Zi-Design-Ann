5e7907507e324ed8f4087b88cb5b05897b24b22a
sadufo <maxzaplitnyy@gmail.com>
2022-09-24 12:57:13 +0300

    ZIDesign ANN

---
+++ ZIDesign/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Header from './components/header/header.vue';
+
+
+</script>
+
+<template>
+    <div class="page__wrapper">
+            <Header/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
