27feb44b0fbea89a3b7b73c58f2340a474256397
sadufo <maxzaplitnyy@gmail.com>
2023-09-06 16:07:50 +0300

    start products

--- ZIDesign/src/components/HomeComponents/aboutUs/aboutUs.vue
+++ ZIDesign/src/components/HomeComponents/aboutUs/aboutUs.vue
@@ -9,8 +9,8 @@ import gStyle from '../../../assets/global_var/global_var.scss'
         <div class="container">
             <div class="aboutUs__item">
                 <div class="aboutUs__img">
+                    <img class="main__img" src="../../../assets/img/aboutUs.png" alt="#">
                     <div class="topbox"></div>
-                    <img src="../../../assets/img/aboutUs.png" alt="#">
                     <div class="swipebox">
                         <h3>27</h3>
                         <p>лет на рынке</p>
@@ -25,21 +25,31 @@ import gStyle from '../../../assets/global_var/global_var.scss'
                         </p>
                     </div>
                     <div class="info__item">
-                        <div class="item__title">
-                            <h4>Качественные материалы</h4>
-                            <h4>Проектирование</h4>
-                        </div>
-                        <div class="item__text">
-                            <p>Качество и безопасность, используемых нами материалов, подтверждаются сертификатами европейского стандарта. 
-                               Мы работаем с материалами, только проверенных годами брендов.
-                            </p>
-                            <p>Не знаете как оформить вашу комнату-мечты?  Все,что нужно от Вас - размеры комнаты и фото,
-                               а наш дизайнер подготовить индвидуальный проект вашего гнёздышка.
-                            </p>
-                        </div>
-                        <div class="item__btn">
+                        <div class="item item-1">
+                            <div class="item__title">
+                                <h4>Качественные материалы</h4>
+                            </div>
+                            <div class="item__text">
+                                <p>Качество и безопасность, используемых нами материалов, подтверждаются сертификатами европейского стандарта. 
+                                   Мы работаем с материалами, только проверенных годами брендов.
+                                </p>
+                            </div>
+                            <div class="item__btn">
                             <button>Подробнее</button>
+                            </div>
+                        </div>
+                        <div class="item item-2">
+                            <div class="item__title">
+                                <h4>Проектирование</h4>
+                            </div>
+                            <div class="item__text">
+                                <p>Не знаете как оформить вашу комнату-мечты?  Все,что нужно от Вас - размеры комнаты и фото,
+                                   а наш дизайнер подготовить индвидуальный проект вашего гнёздышка.
+                                </p>
+                            </div>
+                            <div class="item__btn">
                             <button>Подробнее</button>
+                            </div>
                         </div>
                     </div>
                 </div>
---
+++ ZIDesign/src/view/Catalog/сatalog.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Nav from '../../components/static/nav/nav.vue';
+</script>
+
+<template>
+         <Nav/>
+</template>
